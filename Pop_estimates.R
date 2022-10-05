2245f7988dcc0c8aea6d0e513bf165fcc2202a55
Bronwen-hunter <sh848@sussex.ac.uk>
2022-10-05 15:40:50 +0100

    code edits

--- Pop_estimates.R
+++ Pop_estimates.R
@@ -35,28 +35,60 @@ library(rvest)
 
 # Convert units it Km^2
 
-convert_units <- function(Ha){
-  km2 <- Ha*0.01
-  return (km2)
+convert_units <- function(Ha, species, species_names){
+  if(species_names$density[match(species, species_names$scientific)] == 'Km'){
+    km2 <- Ha*0.01
+    return(km2) 
+  } else{
+    return(Ha)
+  }
 }
 
 # Function to calculate population estimates
-population_esimate <- function(area_df, density_df, species_name){
+population_estimate <- function(area_df, 
+                               density_df,
+                               species_name,
+                               habitat_conversion,
+                               names_df){
   area_subset <- area_df %>% filter(Species == species_name)
-  area_subset <- stack(area_subset[4:29,]) 
+  area_subset <- stack(area_subset[4:29]) 
   names(area_subset) <- c("Projected_Area", "LCM_Broad")
-  area_subset$Projected_Area <- convert_units(as.numeric(area_subset$Projected_Area))
-  results <- left_join(results, area_subset)
-  results$PriorPop <- as.numeric(results$Estimate) * results$AreaTotal
-  results$PriorPop_LowerCI <- as.numeric(results$LowerCI) * results$AreaTotal
-  results$PriorPop_UpperCI <- as.numeric(results$UpperCI) * results$AreaTotal
-  results$HabitatAreaChange <- results$ProjectedArea - as.numeric(results$AreaTotal)
-  results$ProjectedPop <- as.numeric(results$Estimate) * results$ProjectedArea
-  results$ProjectedPop_LowerCI <- results$LowerCI * as.numeric(results$ProjectedArea)
-  results$ProjectedPop_UpperCI <- results$UpperCI * as.numeric(results$ProjectedArea)
-  return(results)
+  area_subset$Projected_Area <- convert_units(area_subset$Projected_Area, species_name, names_df)
+  colnames(density_df)[colnames(density_df) == "UrbanRural"] ="LCM_Broad"
+  area_subset <- merge(area_subset, habitat_conversion, by = 'LCM_Broad')
+  if(species_name == 'Vulpes vulpes'){
+    area_subset$LCM_Broad <- area_subset$very_broad
+  } else if(species_name == 'Mustela erminea'){
+    area_subset$LCM_Broad <- 'All'
+  } else {
+    area_subset$LCM_Broad <- area_subset$density_name
+  }
+  print(nrow(area_subset))
+  area_subset2 <- aggregate(x=area_subset$Projected_Area, by = list(area_subset$LCM_Broad), FUN=sum)
+  names(area_subset2) <- c("LCM_Broad", "Projected_Area")
+  print(area_subset2)
+  results1 <- left_join(density_df, area_subset2)
+  results1$PriorPop <- results1$AreaTotal* results1$Estimate
+  results1$PriorPop_LowerCI <- results1$LowerCI * results1$AreaTotal
+  results1$PriorPop_UpperCI <- results1$UpperCI * results1$AreaTotal
+  results1$HabitatAreaChange <- results1$Projected_Area - results1$AreaTotal
+  results1$ProjectedPop <- results1$Estimate * results1$Projected_Area
+  results1$ProjectedPop_LowerCI <- results1$LowerCI * results1$Projected_Area
+  results1$ProjectedPop_UpperCI <- results1$UpperCI * results1$Projected_Area
+  return(results1)
   }
   
+# Function to calculate total population change from the estimates
+
+calculate_change <- function(estimate_df, species_name){
+  estimate_df <- estimate_df[11:18]
+  estimate_sums <- as.data.frame(t(colSums(estimate_df)))
+  estimate_sums$Species <- species_name
+  return(estimate_sums)
+}
+
+# Function to resolved the names in the LCM 2007 vs 2020 maps
+
 
 # Applying this function to all species and returning new dfs - Wales
 
@@ -64,41 +96,51 @@ produce_all_results <- function(country,
                                 area_df,
                                 listoffiles,
                                 names_df,
+                                habitat_names,
                                 directory){
+  summaryfiles <- list()
   for(item in names(listoffiles)){
     df <- listoffiles[[item]]
-    name <- names_df$scientific[match(item, names_df$common)]
-    results <-population_esimate(area_df, df, name)
+    taxa <- str_split(item, '_')[[1]][1]
+    name <- names_df$scientific[match(taxa, names_df$common)]
+    results <-population_estimate(area_df, df, name, habitat_names)
+    if(names_df$density[match(name, species_names$scientific)] == 'Km'){
+      df$Density_scale <- "Per_Ha"
+      } else {
+      df$Density_scale <- "Per_Km^2"
+      }
     varName <- paste(item, "PopEstimate", country, sep="_")
     assign(varName, results, envir = parent.frame())
     write.csv(results, paste(directory, varName, ".csv"))
-    print(paste("Calculated population estimates for ", item))
+    print(paste("Calculated population estimates for ", name))
+    summary <- calculate_change(results, name)
+    summaryfiles[[name]] <- summary
   }
+  summary_all <- bind_rows(unname(summaryfiles))
+  return(summary_all)
 }
  
-
 ##### RUN THE ANALYSIS HERE ####
 
-# First define the directory where you want the results to be saved
-dir.create('results')
 
 ## First we need to produce a list of density estimates files & read them in: 
 
 DensityFiles <- list()
+
 for(file in list.files('density-estimates')){
-  name <- file # Saving each df based on its name
+  print(file)
+  name <- gsub('.{4}$', '', file) # Saving each df based on its name
   df <- read.csv(paste('density-estimates/', file, sep=""))
   assign(name, df)
   DensityFiles[[name]] <- df # Adding these to a named list
   print(paste("Read in file ", name))
 }
 
-
 # Now the same with the planting simulations for England, Scotland & Wales
 
 AreaFiles <- list()
 for(file in list.files('planting-simulation')){
-  name <- (file) # Saving each df based on its name
+  name <- (gsub('.{4}$', '', file)) # Saving each df based on its name
   df <- read.csv(paste('planting-simulation/', file, sep=''))
   assign(name, df)
   AreaFiles[[name]] <- df # Adding these to a named list
@@ -107,7 +149,12 @@ for(file in list.files('planting-simulation')){
 
 # Reading in the file used to match species names
 species_names <- read.csv('species names.csv')
+habitat_conversion <- read.csv('LCM_conversion.csv')
 
-population_esimate(Final_Wales_Data.csv, Badger_Habitat_180302.csv, 'Meles meles')
+population_estimate(Final_Wales_Data, Fox_Habitat_171107,
+                   'Vulpes vulpes',
+                   habitat_conversion,
+                   species_names) 
 
-
+produce_all_results('Wales', Final_Wales_BizUsual, DensityFiles, species_names, habitat_conversion, 'results/')
+       
